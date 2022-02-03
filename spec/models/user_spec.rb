0436061979ba0992ad1dd4340226fcbd3047cdd1
Felipe <fe.peressim@gmail.com>
2022-02-03 14:00:17 -0300

    Adding rubocop suggestions to spec files

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,18 +4,18 @@ describe User do
   fixtures :users
 
   it 'create a simple when the name is present and is unique, and the age >= 0 when it is present' do
-    User.create(name: "Test name", age: 50, biography: "A biography")
+    User.create(name: 'Test name', age: 50, biography: 'A biography')
     expect(User.count).to eq 2
   end
-  
-  it 'does not create a user when the username is already in the database' do    
-    expect(User.create(name: users( :trainee).name)).not_to be_valid
+
+  it 'does not create a user when the username is already in the database' do
+    expect(User.create(name: users(:trainee).name)).not_to be_valid
   end
 
   it 'does not create a user with empty name' do
     expect(User.create(name: nil)).not_to be_valid
   end
-  
+
   it 'does not create a user when the age is less than zero' do
     expect(User.create(name: 'Another user', age: -1, biography: 'Not a trainee at codeminer')).not_to be_valid
   end
