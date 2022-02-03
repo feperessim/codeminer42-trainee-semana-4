3903fe7b58354b7ce85a7dd6c6cf35625c48698b
Felipe <fe.peressim@gmail.com>
2022-02-03 13:50:25 -0300

    Adding the Contact model and fix some tests for user_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :contacts
   validates :name, presence: true, uniqueness: true
   validates :age, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
 end
