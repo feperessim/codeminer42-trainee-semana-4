95513b86f292b3beb68bd82ef3b5210d28ed5d02
Felipe <fe.peressim@gmail.com>
2022-02-03 14:02:12 -0300

    Adding rubocop suggestions to spec files

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,3 @@
-
 require 'rails_helper'
 
 describe User do
@@ -8,15 +7,15 @@ describe User do
     User.create(name: 'Test name', age: 50, biography: 'A biography')
     expect(User.count).to eq 2
   end
-  
-  it 'does not create a user when the username is already in the database' do    
+
+  it 'does not create a user when the username is already in the database' do
     expect(User.create(name: users(:trainee).name)).not_to be_valid
   end
 
   it 'does not create a user with empty name' do
     expect(User.create(name: nil)).not_to be_valid
   end
-  
+
   it 'does not create a user when the age is less than zero' do
     expect(User.create(name: 'Another user', age: -1, biography: 'Not a trainee at codeminer')).not_to be_valid
   end
